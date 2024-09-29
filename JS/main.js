9957ea6a22a7c458a3ab562519840a0cc25667db
MayarQasrawi <153677880+MayarQasrawi@users.noreply.github.com>
2024-09-29 18:35:38 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,61 @@
-const filterButtons = document.querySelectorAll(".filtterButton button");
-const filterableCards = document.querySelectorAll(".filterable_card .card");
+const products=[
+    {id:1, name:'Shirt',category:'shirts',src:'Picters/Shirt1.jpg',price:'17$'},
+    {id:2, name:'shirt',category:'shirts',src:'Picters/Shirt2.jpg',price:'17$'},
+    {id:3, name:'shirt',category:'shirts',src:'Picters/Shirt3.jpg',price:'19$'},
+    {id:4, name:'shirt',category:'shirts',src:'Picters/Shirt4.jpg',price:'18$'},
+    {id:5, name:'abaya',category:'abaya',src:'Picters/abaya2.jpg',price:'100$'},
+    {id:5, name:'abaya',category:'abaya',src:'Picters/abaya1.jpg',price:'98$'},
+    {id:5, name:'abaya',category:'abaya',src:'Picters/abaya3.jpg',price:'98$'},
+    {id:5, name:'abaya',category:'abaya',src:'Picters/abaya4.jpg',price:'98$'},
+    {id:5, name:'abaya',category:'abaya',src:'Picters/abaya5.jpg',price:'98$'},
+    {id:5, name:'abaya',category:'abaya',src:'Picters/abaya6.jpg',price:'98$'},
+    {id:6, name:'dress',category:'dresses',src:'Picters/dresse1.jpg',price:'100$'},
+    {id:6, name:'dress',category:'dresses',src:'Picters/dresse2.jpg',price:'100$'},
 
-const filterCards = (e) => {
+    
+];
+
+
+const productContainer=document.querySelector('.filterable_card');
+const filterButton = document.querySelector(".filtterButton ");
+//Create a function to display the products
+function displayProduct(array){
+    let list='';
+   list= array.map((product)=> {
+        return `
+                   <div class="card">
+                    
+                    <img src=${product.src} alt=${product.category}>
+                    <div class="card_body">
+                    <h3 >${product.name}</h3>
+                    <p>${product.category}</p>
+                    <p>${product.price}</p>
+                     </div>
+                   </div>
+        `
+    })
+    productContainer.innerHTML=list.join('');
+}
+
+displayProduct(products);
+
+
+
+//Filter the products
+console.log(filterButton);
+filterButton.onclick=function(e){
     document.querySelector(".active").classList.remove("active");
 
     e.target.classList.add("active");
 
-    const filterCategory = e.target.dataset.name;
+    if(e.target.name==='all')
+        displayProduct(products);
+    else{
+        const category=e.target.name;
+        const filteredProducts=products.filter(product=>product.category===category);
+        console.log(filteredProducts);
+        displayProduct(filteredProducts);
+    }
 
-    filterableCards.forEach(card => {
+}
 
-        card.classList.add("hide");
-
-        if (card.dataset.name === filterCategory || filterCategory === "all") {
-            card.classList.remove("hide");
-        }
-    });
-};
-
-filterButtons.forEach(button => button.addEventListener("click", filterCards));
